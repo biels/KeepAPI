c44054266aec786ebb1b11550fae3ee9761673a1
Biel Simon Rojas <bielsimon2@gmail.com>
2016-08-04 21:19:46 +0200

    Set up STS workspace

---
+++ KeepAPI/src/main/java/com/biel/keep/KeepApiApplication.java
@@ -0,0 +1,12 @@
+package com.biel.keep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepApiApplication.class, args);
+	}
+}
---
+++ KeepAPI/src/test/java/com/biel/keep/KeepApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.biel.keep;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KeepApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
