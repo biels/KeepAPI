8493691ba5b43d59f1cb1e5d21a2a5ee41acadd9
Biel Simon Rojas <bielsimon2@gmail.com>
2016-08-04 22:03:33 +0200

    Basic REST controller implementation

--- KeepAPI/src/main/java/com/biel/keep/service/NoteRestController.java
+++ KeepAPI/src/main/java/com/biel/keep/service/NoteRestController.java
@@ -1,8 +1,25 @@
 package com.biel.keep.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.biel.keep.repository.AccountRepository;
+import com.biel.keep.repository.NoteRepository;
+
 @RestController
+@RequestMapping("/{userId}/notes")
 public class NoteRestController {
+	private final AccountRepository accountRepository;
+	private final NoteRepository noteRepository;
+	
+	@Autowired
+	public NoteRestController(AccountRepository accountRepository, NoteRepository noteRepository) {
+		super();
+		this.accountRepository = accountRepository;
+		this.noteRepository = noteRepository;
+	}
+	
+	
 
 }
