f49e8d8844e1982af027486d9a798547d68a5326
Biel Simon Rojas <bielsimon2@gmail.com>
2016-08-05 12:14:58 +0200

    Implemented Account entity

--- KeepAPI/src/main/java/com/biel/keep/model/Account.java
+++ KeepAPI/src/main/java/com/biel/keep/model/Account.java
@@ -1,8 +1,55 @@
 package com.biel.keep.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Account {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	public String username;
+	@JsonIgnore
+	public String password;
+	
+	@OneToMany(mappedBy = "account")
+	private Set<Note> notes = new HashSet<Note>();
+	
+	public Account(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	Account() {
+		// JPA ?
+	}
+
+	public Set<Note> getNotes(){
+		return notes;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	
+	
 
 }
