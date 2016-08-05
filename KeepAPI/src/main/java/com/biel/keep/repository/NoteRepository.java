0b23faf57c5883be7205ae2288ff5447a846ba17
Biel Simon Rojas <bielsimon2@gmail.com>
2016-08-05 12:26:17 +0200

    Implemented Account and Note repositories

--- KeepAPI/src/main/java/com/biel/keep/repository/NoteRepository.java
+++ KeepAPI/src/main/java/com/biel/keep/repository/NoteRepository.java
@@ -1,9 +1,11 @@
 package com.biel.keep.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.biel.keep.model.Note;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
-
+	Collection<Note> findByAccountUsername(String username);
 }
